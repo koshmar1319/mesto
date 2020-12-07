35a2f759660ff81cad52e73855ff8a799cf9b35f
Kirill Komarkov <koshmar1319@gmail.com>
2020-12-07 12:34:42 +0300

    feat: dynamic list rendering

--- script.js
+++ script.js
@@ -43,3 +43,55 @@ function profileClickClose(){
 popupButton.addEventListener('click', profileClickClose => {
   popup.classList.remove('popup_opened');
 });
+
+
+
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const listContainerElement = document.querySelector('.elements__list');
+
+function renderList() {
+  let newHTML = '';
+
+  newHTML = initialCards.map(function(item) {
+    return `<li class="element">
+              <img src="images/grid-karachaevsk.png" alt="Изображение старой церкви" class="element__image">
+              <div class="element__block">
+                <h2 class="element__title">${item.name}</h2>
+                <button class="element__link" type="button">
+                  <img src="images/like.svg" alt="Изображение сердца" class="element__icon">
+                </button>
+              </div>
+            </li>`
+  }).join('');
+  
+  listContainerElement.insertAdjacentHTML('afterbegin', newHTML);
+}
+
+renderList();
